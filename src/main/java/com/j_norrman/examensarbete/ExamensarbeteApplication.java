94e286d3f33ff39d4e1f18a1e8d541a3d2f1083c
Joel Norrman <joel-norrman@hotmail.com>
2024-12-10 02:30:58 +0100

    first commit

---
+++ src/main/java/com/j_norrman/examensarbete/ExamensarbeteApplication.java
@@ -0,0 +1,13 @@
+package com.j_norrman.examensarbete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamensarbeteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamensarbeteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/j_norrman/examensarbete/ExamensarbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j_norrman.examensarbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamensarbeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
